b857b31235c8686326f1195a664043026fe19846
vargabali182-cmyk <vargabali182@gmail.com>
2026-07-09 17:48:49 +0200

    Add files via upload

--- packages/web/src/web/pages/index.tsx
+++ packages/web/src/web/pages/index.tsx
@@ -1,24 +1,28 @@
 import { Navbar } from "../components/navbar";
 import { Hero } from "../components/hero";
+import { ProductStory } from "../components/product-story";
 import { Features } from "../components/features";
 import { Showcase } from "../components/showcase";
 import { Colors } from "../components/colors";
 import { Why } from "../components/why";
+import { InvestorSignal } from "../components/investor-signal";
 import { Waitlist } from "../components/waitlist";
 import { Footer } from "../components/footer";
 
 function Index() {
   return (
-    <div className="no-scrollbar">
+    <main className="min-h-screen bg-[#f5f5f7] text-[#1d1d1f] antialiased selection:bg-[#1d1d1f] selection:text-white">
       <Navbar />
       <Hero />
+      <ProductStory />
       <Features />
       <Showcase />
       <Colors />
       <Why />
+      <InvestorSignal />
       <Waitlist />
       <Footer />
-    </div>
+    </main>
   );
 }
 
